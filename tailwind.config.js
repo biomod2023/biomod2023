481aa5b1f5c7cb9a58f82e7b201b40783a28f242
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-07-16 13:52:55 -0700

    Add icons, configure tailwind presets, and make navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'title': ['Fira Sans', ...defaultTheme.fontFamily.sans]
+    },
+    extend: {
+      colors: {
+        slate: "#272727",
+        gold: "#E2B764",
+      },
+      fontSize: {
+        'title': ['5rem', {
+          fontWeight: '600'
+        }]
+      }
+    },
   },
   plugins: [],
 }
