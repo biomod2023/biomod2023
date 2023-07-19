87d698147dc06dc5b842ff09e7268025ef8ef83f
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-07-19 14:41:24 -0700

    Added footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
 </script>
 
 <template>
-  <NavBar />
-  <main class="pt-2 flex flex-col items-center">
-    <h1 class="text-gold font-title text-title">Title</h1>
-    <p>Text</p>
-  </main>
+  <div class="flex flex-col h-screen">
+    <NavBar />
+    <main class="pt-2 flex flex-col items-center">
+      <h1 class="text-gold font-title text-title">Title</h1>
+      <p>Text</p>
+    </main>
+
+    <div class="flex flex-col justify-end grow">
+      <Footer />
+    </div>
+  </div>
 </template>
