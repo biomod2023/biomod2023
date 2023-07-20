295a3240d36f600ea8c5db180c5307bd7291bef8
Daniel <57362494+daniel-panhead@users.noreply.github.com>
2023-07-20 13:56:55 -0700

    Navbar (#11)
    * Merge tailwind branch
    
    * Configure TailwindCSS
    
    * Add icons, configure tailwind presets, and make navbar
    
    * Prettified code formatting
    
    * changes to readme
    
    * Undid readme changes >:)
    
    * Make nav logo link to homepage
    
    ---------
    
    Co-authored-by: rowelsabahat <115450792+rowelsabahat@users.noreply.github.com>

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,13 +3,17 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex justify-center items-center bg-slate font-semibold text-gold py-3 gap-44 drop-shadow-lg">
+  <div
+    class="flex justify-center items-center bg-slate font-semibold text-gold py-3 gap-44 drop-shadow-lg"
+  >
     <div class="flex justify-center gap-16">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="#">Ideas</RouterLink>
       <RouterLink to="#">Notebook</RouterLink>
     </div>
-    <img src="../assets/logo.svg" alt="Biomod Logo" />
+    <a href="/">
+      <img src="../assets/logo.svg" alt="Biomod Logo" />
+    </a>
     <div class="flex justify-center gap-16">
       <RouterLink to="#">ELSI</RouterLink>
       <RouterLink to="#">Teams</RouterLink>
