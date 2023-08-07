0eab1d125e718a1d788752da58e208baa3ac7817
Minh Au <auduongminh2004@gmail.com>
2023-08-07 13:53:58 -0700

    fix page layout

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -28,42 +28,44 @@
 </script>
 
 <template>
-    <div class="flex flex-col h-max items-center bg-slate">
+    <div class="flex flex-col h-max bg-slate">
         <NavBar/>
 
-        <!-- Header -->
-        <div class="flex flex-col w-screen items-center">
-            <h1 class="text-title text-gold">
-                Our Team
-            </h1> 
-            <h2 class="text-subtitle text-gold">
-                Behold the Amazing Folders!
-            </h2>
-        </div>
-
-        <!-- Team Carousel -->
-        <div class="flex justify-center items-center w-[68.5rem] h-[49.6rem]">
-            <v-icon name="hi-solid-arrow-sm-left" scale="3" @click="handleClick(-1)"/>
-            
-            <!-- Content -->
-            <div class="relative h-full w-full overflow-hidden">
-                <TransitionGroup :enter-from-class="initialState" :leave-to-class="finalState"
-                    :enter-active-class="activeAnimation" 
-                    :leave-active-class="activeAnimation">
-
-                    <div class="flex flex-wrap content-start justify-center h-full gap-x-[6.6rem] gap-y-[2.47rem]" 
-                        v-for="i in [currIdx]" :key="i">
-                        <ProfileCard v-for="member in members.slice(currIdx * maxMembers, (currIdx + 1) * maxMembers)" 
-                            :member="member" :key="member.name"/>
-                    </div>
-                </TransitionGroup>
+        <div class="flex flex-col items-center w-full h-full">
+            <!-- Header -->
+            <div class="flex flex-col w-full items-center">
+                <h1 class="text-title text-gold">
+                    Our Team
+                </h1> 
+                <h2 class="text-subtitle text-gold">
+                    Behold the Amazing Folders!
+                </h2>
             </div>
 
-            <v-icon name="hi-solid-arrow-sm-right" scale="3" @click="handleClick(1)"/>
-        </div>
+            <!-- Team Carousel -->
+            <div class="flex justify-center items-center w-[68.5rem] h-[49.6rem]">
+                <v-icon name="hi-solid-arrow-sm-left" scale="3" @click="handleClick(-1)"/>
+                
+                <!-- Content -->
+                <div class="relative h-full w-full overflow-hidden">
+                    <TransitionGroup :enter-from-class="initialState" :leave-to-class="finalState"
+                        :enter-active-class="activeAnimation" 
+                        :leave-active-class="activeAnimation">
 
+                        <div class="flex flex-wrap content-start justify-center h-full gap-x-[6.6rem] gap-y-[2.47rem]" 
+                            v-for="i in [currIdx]" :key="i">
+                            <ProfileCard v-for="member in members.slice(currIdx * maxMembers, (currIdx + 1) * maxMembers)" 
+                                :member="member" :key="member.name"/>
+                        </div>
+                    </TransitionGroup>
+                </div>
+
+                <v-icon name="hi-solid-arrow-sm-right" scale="3" @click="handleClick(1)"/>
+            </div>
+
+        </div>
         
-        <div class="flex flex-col justify-end grow w-screen">
+        <div class="flex flex-col justify-end grow w-full">
             <Footer />
         </div>
     </div>
