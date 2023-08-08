def11b79d1579ffc191fb7dea9724f0509f7a0c9
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-08-08 13:39:44 -0700

    Fix workflow again again

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import IdeasView from '@/views/IdeasView.vue'
-import TeamView from `@/views/TeamView.vue`
+import TeamView from '@/views/TeamView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
