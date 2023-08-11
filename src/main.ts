9f1c5e94f346e63b62e15ba81cb2fc64326232f5
Daniel <57362494+daniel-panhead@users.noreply.github.com>
2023-08-10 17:35:07 -0700

    New CI stuff (#26)
    * Add ESLint + Vue extensions
    
    * Add CI for lint and build
    
    * Install npm dependencies
    
    * Prettier same commit
    
    * Lint job requires prettier
    
    * Put prettier into lint build workflow
    
    * Eslint (#25)
    
    * Prettify code
    
    * Mess up formatting
    
    * Prettified Code!
    
    * Mess up formatting more
    
    * Mess up formatting more
    
    * Prettified Code!
    
    * Mess up formatting
    
    * Fix lint errors
    
    * Fix lint errors
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: daniel-panhead <daniel-panhead@users.noreply.github.com>
    
    * Prettified Code!
    
    * Move prettify to own job
    
    * Eslint (#27)
    
    * Prettify code
    
    * Mess up formatting
    
    * Prettified Code!
    
    * Mess up formatting more
    
    * Mess up formatting more
    
    * Prettified Code!
    
    * Mess up formatting
    
    * Fix lint errors
    
    * Fix lint errors
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: daniel-panhead <daniel-panhead@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: daniel-panhead <daniel-panhead@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,18 @@ import App from './App.vue'
 import router from './router'
 
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { RiArrowDropLeftLine, RiArrowDropRightLine, HiSolidArrowSmRight, HiSolidArrowSmLeft} from 'oh-vue-icons/icons'
+import {
+  RiArrowDropLeftLine,
+  RiArrowDropRightLine,
+  HiSolidArrowSmRight,
+  HiSolidArrowSmLeft
+} from 'oh-vue-icons/icons'
 
 addIcons(HiSolidArrowSmLeft, HiSolidArrowSmRight, RiArrowDropRightLine, RiArrowDropLeftLine)
 
 const app = createApp(App)
 
 app.use(router)
-app.component('v-icon', OhVueIcon)
+app.component('VIcon', OhVueIcon)
 
 app.mount('#app')
