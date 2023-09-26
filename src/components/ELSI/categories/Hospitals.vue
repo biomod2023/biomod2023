a9f7e9da8d8737e54728083a2f0482e53f0f2c85
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-09-26 14:56:24 -0700

    Add transition to elsi gallery

---
+++ src/components/ELSI/categories/Hospitals.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-[2rem] lg:text-[3rem] font-semibold font-title">Issues for Doctors</h1>
+        <h2 class="text-subtitle-sm lg:text-subtitle">Concerns</h2>
+        <p>
+            This includes the ethical implications of using a new drug that has not been tested for long-term side effects on patients.
+            In order to be able to use the enzymosomes to degrade bacterial biofilms in patients, there will be testing prior to application
+            clinically but there may still be ethical considerations and differences between different medical practitioners willingness to
+            administer a new drug to patients with little known long-term effects.
+        </p>
+        <h2 class="text-subtitle-sm lg:text-subtitle mt-4">Concerns</h2>
+        <p>
+            This includes the ethical implications of using a new drug that has not been tested for long-term side effects on patients.
+            In order to be able to use the enzymosomes to degrade bacterial biofilms in patients, there will be testing prior to application
+            clinically but there may still be ethical considerations and differences between different medical practitioners willingness to
+            administer a new drug to patients with little known long-term effects.
+        </p>
+        <h2 class="text-subtitle-sm lg:text-subtitle mt-4">Concerns</h2>
+        <p>
+            This includes the ethical implications of using a new drug that has not been tested for long-term side effects on patients.
+            In order to be able to use the enzymosomes to degrade bacterial biofilms in patients, there will be testing prior to application
+            clinically but there may still be ethical considerations and differences between different medical practitioners willingness to
+            administer a new drug to patients with little known long-term effects.
+        </p>
+    </div>
+</template>
--- src/views/ELSIView.vue
+++ src/views/ELSIView.vue
@@ -1,9 +1,57 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import ELSICardGallery from '@/components/ELSI/ELSICardGallery.vue'
-import Doctors from '@/components/ELSI/Doctors.vue'
+import Hospitals from '@/components/ELSI/categories/Hospitals.vue'
+import Universities from '@/components/ELSI/categories/Universities.vue'
+import Agriculture from '@/components/ELSI/categories/Agriculture.vue'
+import Home from '@/components/ELSI/categories/Home.vue'
+import ResearchFacilities from '@/components/ELSI/categories/ResearchFacilities.vue'
+import HospitalLab from '@/components/ELSI/categories/HospitalLab.vue'
+import Businesses from '@/components/ELSI/categories/Businesses.vue'
+
+const categories = [
+  {
+    name: "Hospitals",
+    component: Hospitals
+  },
+  {
+    name: "Universities",
+    component: Universities
+  },
+  {
+    name: "Agriculture",
+    component: Agriculture
+  },
+  {
+    name: "Home",
+    component: Home
+  },
+  {
+    name: "Research Facilities",
+    component: ResearchFacilities
+  },
+  {
+    name: "Hospital Lab",
+    component: HospitalLab
+  },
+  {
+    name: "Businesses",
+    component: Businesses
+  }
+]
 
 let currentSelection = ref(0);
+let increasing = ref(true)
+
+const fromRightAnim = "translate-x-1/4 opacity-0"
+const fromLeftAnim = "-translate-x-1/4 opacity-0"
+const activeAnim="transition duration-200 ease-in-out"
+
+const setSelection = (num: number) => {
+  if (num >= currentSelection.value) increasing.value = true
+  else increasing.value = false
+  currentSelection.value = num
+}
 </script>
 
 <template>
@@ -13,15 +61,30 @@ let currentSelection = ref(0);
         Ethical, Legal, and Social Issues
       </h1>
 
-      <ELSICardGallery @current-selection="(num) => currentSelection = num" />
+      <ELSICardGallery @current-selection="setSelection" />
 
-      <div class="w-full flex flex-col py-8 px-8 md:px-10 xl:px-32">
-        <template v-if="currentSelection == 0">
-          <h1 class="text-title-sm lg:text-title text-gray-300 pl-8 pb-2">Hospitals</h1>
-          <div class="flex flex-col justify-center w-full text-black bg-gray-300 rounded-[3em] p-8 m-auto">
-            <Doctors />
-          </div>
-        </template>
+      <div class="w-full flex flex-col py-8 px-8 md:px-10 xl:px-32 overflow-hidden">
+        <Transition
+          mode="out-in"
+          :enter-from-class="increasing ? fromRightAnim : fromLeftAnim"
+          :leave-to-class="increasing ? fromLeftAnim : fromRightAnim"
+          :enter-active-class="activeAnim"
+          :leave-active-class="activeAnim"
+        >
+          <h1 :key="currentSelection" class="text-title-sm lg:text-title text-gray-300 pl-8 pb-2">{{ categories[currentSelection].name }}</h1>
+        </Transition>
+        
+        <div class="flex flex-col justify-center w-full text-black bg-gray-300 rounded-[3em] p-8 m-auto">
+          <Transition
+            mode="out-in"
+            :enter-from-class="increasing ? fromRightAnim : fromLeftAnim"
+            :leave-to-class="increasing ? fromLeftAnim : fromRightAnim"
+            :enter-active-class="activeAnim"
+            :leave-active-class="activeAnim"
+          >
+            <component :is="categories[currentSelection].component"></component>
+          </Transition>
+        </div>
       </div>
 
     </div>
