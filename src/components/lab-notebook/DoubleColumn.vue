1e8a4721739a76dab1c39be07aba3b00a856f95a
daniel-panhead <daniel-panhead@users.noreply.github.com>
2023-09-29 00:30:08 +0000

    Prettified Code!

--- src/components/lab-notebook/DoubleColumn.vue
+++ src/components/lab-notebook/DoubleColumn.vue
@@ -11,14 +11,12 @@ onMounted(() => {
   if (title.value) {
     id.value = title.value.textContent.trim()
   }
-
 })
 
 const onScroll = () => {
   if (scrollWindow.value?.scrollLeft == 0) scrollPos.value = 0
   if (scrollWindow.value?.scrollLeft == 247) scrollPos.value = 247
 }
-
 </script>
 
 <template>
@@ -28,7 +26,11 @@ const onScroll = () => {
     </template>
     <template #body>
       <div class="flex flex-col items-center gap-4">
-        <div ref="scrollWindow" @scroll="onScroll" class="flex gap-8 w-full overflow-x-scroll snap-x snap-mandatory lg:overflow-x-auto">
+        <div
+          ref="scrollWindow"
+          @scroll="onScroll"
+          class="flex gap-8 w-full overflow-x-scroll snap-x snap-mandatory lg:overflow-x-auto"
+        >
           <div
             class="min-w-full lg:min-w-0 lg:w-1/2 text-notebookText bg-slate p-6 rounded-[3.2em] snap-center"
           >
@@ -47,8 +49,20 @@ const onScroll = () => {
           </div>
         </div>
         <div v-if="$windowWidth < 1024" class="flex justify-center gap-4">
-          <span :class="{'bg-notebookText': scrollPos == 0, 'border border-notebookText': scrollPos != 0 }" class="w-2 h-2 rounded-full transition duration-500"></span>
-          <span :class="{ 'bg-notebookText': scrollPos != 0, 'border border-notebookText': scrollPos == 0 }" class=" w-2 h-2 rounded-full transition duration-500"></span>
+          <span
+            :class="{
+              'bg-notebookText': scrollPos == 0,
+              'border border-notebookText': scrollPos != 0
+            }"
+            class="w-2 h-2 rounded-full transition duration-500"
+          ></span>
+          <span
+            :class="{
+              'bg-notebookText': scrollPos != 0,
+              'border border-notebookText': scrollPos == 0
+            }"
+            class="w-2 h-2 rounded-full transition duration-500"
+          ></span>
         </div>
       </div>
     </template>
