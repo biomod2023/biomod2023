298f8b3d4cc2b6e852fceac3c8cd1921edf31d47
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-09-28 19:13:01 -0700

    Add triplecolumn

--- src/components/lab-notebook/TripleColumn.vue
+++ src/components/lab-notebook/TripleColumn.vue
@@ -6,17 +6,20 @@ const title = ref()
 const id = ref('')
 const scrollWindow = ref<HTMLElement>()
 const scrollPos = ref(0)
+let maxWidth = 0
 
 onMounted(() => {
   if (title.value) {
     id.value = title.value.textContent.trim()
   }
+  maxWidth = scrollWindow.value ? (scrollWindow.value?.scrollWidth - scrollWindow.value?.clientWidth) : 0
 })
 
-const maxWidth = scrollWindow.value ? (scrollWindow.value?.scrollWidth - scrollWindow.value?.clientWidth) : 0
+
 
 const onScroll = () => {
   if (scrollWindow.value?.scrollLeft == 0) scrollPos.value = 0
+  if (scrollWindow.value?.scrollLeft == maxWidth/2) scrollPos.value = maxWidth/2
   if (scrollWindow.value?.scrollLeft == maxWidth) scrollPos.value = maxWidth
 }
 </script>
@@ -34,7 +37,7 @@ const onScroll = () => {
           class="flex gap-8 w-full overflow-x-scroll snap-x snap-mandatory lg:overflow-x-auto"
         >
           <div
-            class="min-w-full lg:min-w-0 lg:w-1/2 text-notebookText bg-slate p-6 rounded-[3.2em] snap-center"
+            class="min-w-full lg:min-w-0 lg:w-1/3 text-notebookText bg-slate p-6 rounded-[3.2em] snap-center"
           >
             <h3 class="text-subtitle-sm lg:text-subtitle text-white mb-4">
               <slot name="left-title"></slot>
@@ -42,7 +45,15 @@ const onScroll = () => {
             <slot name="left-body"></slot>
           </div>
           <div
-            class="min-w-full lg:min-w-0 lg:w-1/2 text-notebookText bg-slate p-6 rounded-[3.2em] snap-center"
+              class="min-w-full lg:min-w-0 lg:w-1/3 text-notebookText bg-slate p-6 rounded-[3.2em] snap-center"
+            >
+              <h3 class="text-subtitle-sm lg:text-subtitle text-white mb-4">
+                <slot name="mid-title"></slot>
+              </h3>
+              <slot name="mid-body"></slot>
+            </div>
+          <div
+            class="min-w-full lg:min-w-0 lg:w-1/3 text-notebookText bg-slate p-6 rounded-[3.2em] snap-center"
           >
             <h3 class="text-subtitle-sm lg:text-subtitle text-white mb-4">
               <slot name="right-title"></slot>
@@ -60,11 +71,18 @@ const onScroll = () => {
           ></span>
           <span
             :class="{
+              'bg-notebookText': scrollPos == maxWidth/2,
+              'border border-notebookText': scrollPos != maxWidth/2
+            }"
+            class="w-2 h-2 rounded-full transition duration-500"
+          ></span>
+          <span
+            :class="{
               'bg-notebookText': scrollPos == maxWidth,
               'border border-notebookText': scrollPos != maxWidth
             }"
             class="w-2 h-2 rounded-full transition duration-500"
-          ></span>
+            ></span>
         </div>
       </div>
     </template>
