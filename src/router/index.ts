a2fbc0199781bc225e5d51904fcd6c564b1422e6
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-10-03 14:15:34 -0700

    Finally remove that default about page

--- src/router/index.ts
+++ src/router/index.ts
@@ -35,14 +35,6 @@ const router = createRouter({
       path: '/enzymosome/enzyme-activity-assays',
       name: 'enzyme-activity-assays',
       component: GroupAView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
   ]
 })
