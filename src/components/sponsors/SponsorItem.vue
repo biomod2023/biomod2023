e723171722830df4a58116ccc77fcb237ae7f9f6
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-10-21 17:59:15 -0700

    Add gold + silver sponsors with backgrounds and bronze sponsor title

---
+++ src/components/sponsors/SponsorItem.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col lg:flex-row items-center gap-8 lg:gap-4 lg:w-5/6 my-6 lg:my-20 text-center lg:text-start">
+    <div class="lg:basis-1/2">
+      <div class="m-auto px-8 md:px-20 lg:px-8 xl:px-10 relative hover:scale-105 cursor-pointer transition-transform duration-200">
+        <slot name="img"></slot>
+      </div>
+    </div>
+    <div class="basis-1/2">
+      <h2 class="text-3xl font-semibold mb-2">
+        <slot name="title"></slot>
+      </h2>
+      <p class="text-lg">
+        <slot name="description"></slot>
+      </p>
+    </div>
+  </div>
+</template>
--- src/views/SponsorsView.vue
+++ src/views/SponsorsView.vue
@@ -1,9 +1,67 @@
 <script setup lang="ts">
-
+import SponsorItem from '@/components/sponsors/SponsorItem.vue';
+import SponsorTier from '@/components/sponsors/SponsorTier.vue';
+import SilverImgBackground from '@/components/sponsors/SilverImgBackground.vue';
 </script>
 
 <template>
-    <div class="flex flex-col items-center p-8">
-        <h1 class="text-title-sm lg:text-title text-gold">Special Thanks to our Sponsors</h1>
-    </div>
+  <div class="flex flex-col items-center p-8 gap-12 bg-[url('../assets/sponsors/dna.png')] bg-no-repeat bg-contain bg-[center_top_58%] lg:bg-[center_top_65%]">
+    <h1 class="text-title-sm lg:text-title text-gold mb-6 lg:mb-20 text-center">Special Thanks to our Sponsors</h1>
+
+    <SponsorTier color="bg-gold">
+      Gold Tier
+    </SponsorTier>
+    <SponsorItem>
+      <template #img>
+        <span class="absolute overflow-hidden -top-[20vw] md:-top-[15vw] lg:-top-[12vw] xl:-top-[9vw] -ml-[43vw] md:-ml-[35vw] lg:-ml-[8vw] xl:-ml-[4vw]">
+          <SilverImgBackground />
+        </span>
+        <a href="https://abcellera.com/" target="_blank" class="relative z-10 hover:opacity-100">
+          <img src="../assets/sponsors/abcellera.jpg" alt="AbCellera Logo">
+        </a>       
+      </template>
+      <template #title>
+        AbCellera
+      </template>
+      <template #description>
+        AbCellera is breaking the barriers of conventional antibody drug discovery to bring better medicines to patients,
+        sooner. AbCellera’s engine integrates expert teams, technology, and facilities with the data science and
+        automation needed to propel antibody-based medicines from target to clinic in nearly every therapeutic area with
+        precision and speed. AbCellera provides innovative biotechs and leading pharmaceutical companies with a
+        competitive advantage that empowers them to move quickly, reduce cost, and tackle the toughest problems in drug
+        development.
+      </template>
+    </SponsorItem>
+
+    <SponsorTier color="bg-gray-300">
+      Silver Tier
+    </SponsorTier>
+    <SponsorItem>
+      <template #img>
+        <a href="https://vpstudents.ubc.ca/walter-gage/" target="_blank" class="hover:opacity-100">
+          <img src="../assets/sponsors/walter_gage.png" alt="Walter Gage Logo">
+        </a>
+      </template>
+      <template #title>
+          Walter H. Gage Memorial Fund
+        </template>
+      <template #description>
+        The Walter H. Gage Memorial Fund provides financial support for non-curricular student projects and activities that enhance the University’s reputation and have broad interest to students at UBC.
+      </template>
+    </SponsorItem>
+
+    <SponsorTier color="bg-yellow-600">
+      Bronze Tier
+    </SponsorTier>
+    <SponsorItem>
+      <template #img>
+        <img src="../assets/sponsors/walter_gage.png" alt="Walter Gage Logo">
+      </template>
+      <template #description>
+        The Walter H. Gage Memorial Fund provides financial support for non-curricular student projects and activities that enhance the University’s reputation and have broad interest to students at UBC.
+      </template>
+    </SponsorItem>
+
+    
+  </div>
 </template>
