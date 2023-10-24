b236af327e037e1e82bf18665a5b27d2fb493bb6
Minh Au <auduongminh2004@gmail.com>
2023-10-23 23:53:21 -0700

    equation can now go into display mode (for multiline equations)

--- src/components/Equation.vue
+++ src/components/Equation.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
 import katex from 'katex'
 import { type Ref, ref, onMounted } from 'vue'
-const props = defineProps<{
-  formula: string
-}>()
-const containerRef = ref(null) as unknown as Ref<HTMLElement>
+const props = withDefaults(defineProps<{
+  formula: string,
+  displayMode?: boolean
+}>(), {
+  formula : "",
+  displayMode: false
+})
 
+const containerRef = ref(null) as unknown as Ref<HTMLElement>
 onMounted(() => {
-  katex.render(props.formula, containerRef.value, { throwOnError: false })
+  katex.render(props.formula, containerRef.value, 
+    { 
+      throwOnError: false, 
+      displayMode: props.displayMode
+    })
 })
 </script>
 <template>
-  <div
+  <span
     class="w-full overflow-x-scroll whitespace-nowrap bg-transparent invisible-scroller text-center"
     ref="containerRef"
-  ></div>
+  ></span>
 </template>
