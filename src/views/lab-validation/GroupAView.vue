7be99233460de37e4c82adcdcfebe6c1b044968f
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-10-23 22:20:55 -0700

    Added new content and fixed formatting

--- src/views/lab-validation/GroupAView.vue
+++ src/views/lab-validation/GroupAView.vue
@@ -22,6 +22,8 @@ const tableData = {
     [0.1, 50, 20]
   ]
 }
+
+const sectionTitleStyle = 'text-subtitle-sm lg:text-subtitle text-white mb-4'
 </script>
 
 <template>
@@ -31,18 +33,20 @@ const tableData = {
       <SingleColumn>
         <template #title> Intro </template>
         <template #body>
-          <p>
-            Prior to using any enzymes in creating an enzymosome, the baseline activity of each
-            enzyme in different solvent contexts must be determined to control for the effects of
-            various solvents on enzyme activity.
-          </p>
-          <br />
-          <p>
-            The enzyme DNase I has been selected for enzymosome creation, as it plays a key role in
-            degrading biofilms. DNase I disrupts bacterial biofilms by degrading the extracellular
-            DNA within the biofilm, which can be used as a nutrient source by the bacteria that
-            produce the biofilm (Nijland et al., 2010).
-          </p>
+          <div class="flex flex-col gap-y-4">
+            <p>
+              Prior to using any enzymes in creating an enzymosome, the baseline activity of each enzyme in different
+              solvent contexts must be determined to control for the effects of various solvents on enzyme activity. This
+              is
+              important for determining whether they will interfere with enzyme function, as well as which solvent is best
+              for working with the enzyme.
+            </p>
+            <p>
+              The enzyme DNase I has been selected for enzymosome creation, as it plays a key role in degrading biofilms.
+              DNase I disrupts bacterial biofilms by degrading the extracellular DNA within the biofilm, which can be used
+              as a nutrient source by the bacteria that produce the biofilm (Nijland et al., 2010).
+            </p>
+          </div>
         </template>
       </SingleColumn>
       <DoubleColumn>
@@ -70,37 +74,38 @@ const tableData = {
         <template #title> Methods </template>
         <template #body>
           <h3 class="text-subtitle-sm lg:text-subtitle text-white mb-4">DNA I Assay</h3>
-          <p>
-            The DNase I assay was adapted from the standard protocol of the Picogreen Assay kit
-            (ThermoFischer Scientific, 2022). This kit uses a reaction buffer containing 1 M
-            Tris-HCl (pH 7.5), 1 M MgCl2, and 1 M CaCl2 (ThermoFischer Scientific, 2022), which will
-            be referred to as the manufacturer’s buffer. 0.05 U/mL and 0.1 U/mL DNase I solutions
-            were prepared in the manufacturer’s buffer and the HEPES buffer separately for use in
-            the activity assay. 0.05 U/mL and 0.1 U/mL concentrations of DNase I in HEPES buffer
-            contained 55mM CaCl₂ and 10mM MgCl. The HEPES buffer was also used for thiolation
-            treatment and conjugation to the liposome.
-          </p>
-          <br />
-          <p>
-            1 μM DNA Probe was added to six wells of the 96 well plate to generate 0, 4, 8, 12, 16,
-            and 20 pmol/well standards of 50 μL total volume. The manufacturer’s buffer, the HEPES
-            buffer, and DNase I with the manufacturer’s buffer and DNase I with the HEPES buffer
-            were added to separate wells of the 96-well plate, with each solution being added to two
-            wells and 50 μL of solution in each well. These were the negative and experimental
-            controls. 50 μL of molecular biology grade water was also added to two wells as a
-            background control. 50 μL of only Sample Reaction Mix was also added to two wells as a
-            positive control. 50 μL of Sample Reaction Mix was also added to each of the negative,
-            background, experimental and positive control wells for a total of 100μL of solution in
-            each well. Fluorescence was measured at an excitation wavelength of about 480 nm and an
-            emission wavelength of about 520 nm to determine enzyme activity using a
-            fluorometer-spectrophotometer in kinetic mode every minute for at least 10 minutes.
-          </p>
+          <div class="flex flex-col gap-y-4">
+            <p>
+              The DNase I assay was adapted from the standard protocol of the Picogreen Assay kit
+              (ThermoFischer Scientific, 2022). This kit uses a reaction buffer containing 1 M
+              Tris-HCl (pH 7.5), 1 M MgCl2, and 1 M CaCl2 (ThermoFischer Scientific, 2022), which will
+              be referred to as the manufacturer’s buffer. 0.05 U/mL and 0.1 U/mL DNase I solutions
+              were prepared in the manufacturer’s buffer and the HEPES buffer separately for use in
+              the activity assay. 0.05 U/mL and 0.1 U/mL concentrations of DNase I in HEPES buffer
+              contained 55mM CaCl₂ and 10mM MgCl. The HEPES buffer was also used for thiolation
+              treatment and conjugation to the liposome.
+            </p>
+            <p>
+              1 μM DNA Probe was added to six wells of the 96 well plate to generate 0, 4, 8, 12, 16,
+              and 20 pmol/well standards of 50 μL total volume. The manufacturer’s buffer, the HEPES
+              buffer, and DNase I with the manufacturer’s buffer and DNase I with the HEPES buffer
+              were added to separate wells of the 96-well plate, with each solution being added to two
+              wells and 50 μL of solution in each well. These were the negative and experimental
+              controls. 50 μL of molecular biology grade water was also added to two wells as a
+              background control. 50 μL of only Sample Reaction Mix was also added to two wells as a
+              positive control. 50 μL of Sample Reaction Mix was also added to each of the negative,
+              background, experimental and positive control wells for a total of 100μL of solution in
+              each well. Fluorescence was measured at an excitation wavelength of about 480 nm and an
+              emission wavelength of about 520 nm to determine enzyme activity using a
+              fluorometer-spectrophotometer in kinetic mode every minute for at least 10 minutes.
+            </p>
+          </div>
         </template>
       </SingleColumn>
       <SingleColumn>
         <template #title>Results</template>
-        <template #graphic>
-          <div class="flex flex-col gap-8 w-full">
+        <template #body>
+          <div class="flex flex-col items-center gap-8 w-full mb-4">
             <div class="flex flex-col lg:flex-row lg:justify-center items-center gap-6">
               <div class="basis-1/2">
                 <img src="../../assets/dnase-i/manu-buffer.png" alt="Graph of Manufacturer Buffer">
@@ -112,7 +117,7 @@ const tableData = {
             <div class="w-full">
               <img src="../../assets/dnase-i/hepes-buffer.png" alt="Graph of HEPES Buffer" class="lg:w-1/2 m-auto">
             </div>
-            <p>
+            <p class="text-center text-sm">
               Figure 1: Exhibits the DNase I Picogreen Assay in Manufacturer, HEPES, and TE buffers. It displays the
               generated DNA concentration in testing the activity of DNase I in different buffers. Panels a, b, and c
               display Manufacturer, HEPES, and TE buffers respectively.
@@ -121,28 +126,36 @@ const tableData = {
               <div class="overflow-x-scroll w-full">
                 <CustomTable :table-data="tableData" />
               </div>
-              <p>
+              <p class="text-center text-sm">
                 Table 1: Approximate concentration of DNase I in each buffer at the beginning and
                 end of the fluorescence measuring period, based on the combined information of
                 Figure 1 and Figure 2.
               </p>
             </div>
           </div>
+          <p>
+            Figure 1 demonstrates the DNA concentration over time in solutions with various concentrations of DNAse I
+            and various buffers. This concentration appears to be relatively constant in the TE buffer, while decreasing
+            gradually over time in the manufacturer’s buffer and the HEPES buffer.
+          </p>
         </template>
       </SingleColumn>
 
       <SingleColumn>
         <template #title>Discussion</template>
         <template #body>
-          <div class="flex flex-col gap-4">
+          <div class="flex flex-col gap-4 mb-4">
             <p>
-              Since DNase I is an enzyme that degrades DNA (Nijland et al., 2010), the concentration
-              of DNA determined from Figures 1 and 2 demonstrates that DNase I has minimal activity
-              in the TE buffer and a relatively high amount of activity in the manufacturer's buffer
-              and HEPES buffer. These results are supported because the EDTA in the TE buffer
-              inactivates DNase by chelating with the metal cations it needs to function (Panda et
-              al., 2019), causing less DNase I activity in the TE buffer than in the other buffers.
+              Since DNase I is an enzyme that degrades DNA (Nijland et al., 2010), the concentration of DNA determined
+              from Figure 1 demonstrates that DNase I has minimal activity in the TE buffer and a relatively high amount
+              of activity in the manufacturer's buffer and HEPES buffer. These results are supported because the EDTA in
+              the TE buffer inactivates DNase by chelating with the metal cations it needs to function (Panda et al.,
+              2019), causing less DNase I activity in the TE buffer than in the other buffers.
             </p>
+          </div>
+
+          <h3 :class="sectionTitleStyle">Future Directions</h3>
+          <div class="flex flex-col gap-4">
             <p>
               While only DNAse I has been selected, a combination of enzymes would ideally be used
               in conjunction on the enzymosome to combat bacterial biofilms. For example, alpha
@@ -183,42 +196,44 @@ const tableData = {
         <template #body>
           <div class="pl-6 -indent-6">
             <p>
-              Dische, Z. (1953). Qualitative and quantitative colorimetric determination of
-              heptoses. Journal of Biological Chemistry, 204(2), 983–997.
+              Dische, Z. (1953). Qualitative and quantitative colorimetric determination of heptoses. Journal of
+              Biological Chemistry, 204(2), 983–997. https://doi.org/10.1016/S0021-9258(18)66101-0
             </p>
             <p>
-              Lahiri, D., Nag, M., Banerjee, R., Mukherjee, D., Garai, S., Sarkar, T., Dey, A.,
-              Sheikh, H. I., Pathak, S. K., & Edinur, H. A. (2021). Amylases: Biofilm inducer or
-              biofilm inhibitor? Frontiers in Cellular and Infection Microbiology, 11, 660048.
+              Lahiri, D., Nag, M., Banerjee, R., Mukherjee, D., Garai, S., Sarkar, T., Dey, A., Sheikh, H. I., Pathak, S.
+              K., & Edinur, H. A. (2021). Amylases: Biofilm inducer or biofilm inhibitor? Frontiers in Cellular and
+              Infection Microbiology, 11, 660048. https://doi.org/10.3389/fcimb.2021.660048
             </p>
             <p>
-              Nijland, R., Hall, M. J., & Burgess, J. G. (2010). Dispersal of biofilms by secreted,
-              matrix degrading, bacterial DNase. PloS One, 5(12), e15668.
+              Nijland, R., Hall, M. J., & Burgess, J. G. (2010). Dispersal of biofilms by secreted, matrix degrading,
+              bacterial DNase. PloS One, 5(12), e15668. https://doi.org/10.1371/journal.pone.0015668
             </p>
             <p>
-              Panda, B. B., Meher, A. S., & Hazra, R. K. (2019). Comparison between different
-              methods of DNA isolation from dried blood spots for determination of malaria to
-              determine specificity and cost effectiveness. Journal of Parasitic Diseases, 43(3),
-              337–342.
+              Panda, B. B., Meher, A. S., & Hazra, R. K. (2019). Comparison between different methods of DNA isolation
+              from dried blood spots for determination of malaria to determine specificity and cost effectiveness. Journal
+              of Parasitic Diseases, 43(3), 337–342. https://doi.org/10.1007/s12639-019-01136-0
             </p>
             <p>
-              Preiss, J., & Ashwell, G. (1962). Alginic acid metabolism in bacteria: I. Enzymatic
-              formation of unsaturated oligosaccharides and 4-deoxy-L-erythro-5-hexoseulose uronic
-              acid. Journal of Biological Chemistry, 237(2), 309–316.
+              Preiss, J., & Ashwell, G. (1962). Alginic acid metabolism in bacteria: I. Enzymatic formation of unsaturated
+              oligosaccharides and 4-deoxy-L-erythro-5-hexoseulose uronic acid. Journal of Biological Chemistry, 237(2),
+              309–316. https://doi.org/10.1016/S0021-9258(18)93920-7
             </p>
-            <p>Scopes, R. K. (2001). Enzyme activity and assays. E LS.</p>
+            <p>
+              Scopes, R. K. (2001). Enzyme activity and assays. Encyclopedia of Life Sciences.
+              https://doi.org/10.1038/npg.els.0000712
+            </p>
             <p>
               Sigma-Aldrich. (2014). Amylase Activity Assay Kit.
               https://www.sigmaaldrich.com/deepweb/assets/sigmaaldrich/product/documents/377/793/mak009bul.pdf
             </p>
             <p>
-              ThermoFischer Scientific. (2022). Quant-iTTM PicoGreenTM dsDNA Reagent and Kit.
+              ThermoFisher Scientific. (2022). Quant-iTTM PicoGreenTM dsDNA Reagent and Kit.
               https://www.thermofisher.com/document-connect/document-connect.html?url=https://assets.thermofisher.com/TFS-Assets%2FLSG%2Fmanuals%2Fmp07581.pdf
             </p>
             <p>
-              Zhu, B., & Yin, H. (2015). Alginate lyase: Review of major sources and classification,
-              properties, structure-function analysis and applications. Bioengineered, 6(3),
-              125–131.
+              Zhu, B., & Yin, H. (2015). Alginate lyase: Review of major sources and classification, properties,
+              structure-function analysis and applications. Bioengineered, 6(3), 125–131.
+              https://doi.org/10.1080/21655979.2015.1030543
             </p>
           </div>
         </template>
