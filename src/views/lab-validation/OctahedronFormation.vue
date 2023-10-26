76575d67703dae0b0bb9fa96c337fbf8598af04d
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-10-25 17:59:55 -0700

    Make methods table caption smaller to be consistent + wrap with gap-y-4 to give spacing between caption and rest of text

--- src/views/lab-validation/OctahedronFormation.vue
+++ src/views/lab-validation/OctahedronFormation.vue
@@ -77,79 +77,80 @@ const tableData = {
       <SingleColumn>
         <template #title> Methods </template>
         <template #body>
-          <p>
-            We began by preparing various samples with different scaffold:staple ratios to
-            investigate their impact on structure formation. The ratios included 1:1, 1:2, 1:3, 1:5,
-            1:7, and 1:10. To create these samples, we first produced a 1000 nM master mix of staple
-            strands based on the work done by Perrault & Shih (2016). This master mix included the
-            following components: 432 μL of the 'Core' oligo pool, 288 μL of the 'Outer Handle'
-            oligo pool, and 144 μL of the 'Inner Handle' oligo pool (Perrault & Shih, 2016).
-          </p>
-          <br />
-          <p>
-            Subsequently, for each of the scaffold:staple ratios, we combined the appropriate
-            volumes of the reagents as outlined in Table 1 into PCR tubes.
-          </p>
-          <div class="overflow-x-scroll">
-            <table class="table-auto border-collapse lg:w-full">
-              <tr class="bg-slate h-12">
-                <th colspan="7">Volumes for each Ratio (μL)</th>
-              </tr>
-              <tbody class="bg-dark">
-                <tr v-for="(row, i) in tableData.data.slice(0, -1)" :key="i">
-                  <th scope="row" class="w-1/4">
-                    {{ row[0] }}
-                  </th>
-                  <td
-                    v-for="(item, j) in row.slice(1)"
-                    :key="j"
-                    class="py-1 lg:text-lg text-center"
-                  >
-                    {{ item }}
-                  </td>
+          <div class="flex flex-col gap-y-4">
+            <p>
+              We began by preparing various samples with different scaffold:staple ratios to
+              investigate their impact on structure formation. The ratios included 1:1, 1:2, 1:3, 1:5,
+              1:7, and 1:10. To create these samples, we first produced a 1000 nM master mix of staple
+              strands based on the work done by Perrault & Shih (2016). This master mix included the
+              following components: 432 μL of the 'Core' oligo pool, 288 μL of the 'Outer Handle'
+              oligo pool, and 144 μL of the 'Inner Handle' oligo pool (Perrault & Shih, 2016).
+            </p>
+            <br />
+            <p>
+              Subsequently, for each of the scaffold:staple ratios, we combined the appropriate
+              volumes of the reagents as outlined in Table 1 into PCR tubes.
+            </p>
+            <div class="overflow-x-scroll">
+              <table class="table-auto border-collapse lg:w-full">
+                <tr class="bg-slate h-12">
+                  <th colspan="7">Volumes for each Ratio (μL)</th>
                 </tr>
-              </tbody>
-              <tfoot class="bg-slate">
-                <tr>
-                  <th scope="row">{{ tableData.data[tableData.data.length - 1][0] }}</th>
-                  <td
-                    v-for="(item, j) in tableData.data[tableData.data.length - 1].slice(
-                      1,
-                      tableData.data.length
-                    )"
-                    :key="j"
-                    class="py-1 lg:text-lg text-center"
-                  >
-                    {{ item }}
-                  </td>
-                </tr>
-              </tfoot>
-              <caption align="bottom">
-                Table 1. Reagent Volumes for Scaffold:Staple Ratios.
-              </caption>
-            </table>
+                <tbody class="bg-dark">
+                  <tr v-for="(row, i) in tableData.data.slice(0, -1)" :key="i">
+                    <th scope="row" class="w-1/4">
+                      {{ row[0] }}
+                    </th>
+                    <td
+                      v-for="(item, j) in row.slice(1)"
+                      :key="j"
+                      class="py-1 lg:text-lg text-center"
+                    >
+                      {{ item }}
+                    </td>
+                  </tr>
+                </tbody>
+                <tfoot class="bg-slate">
+                  <tr>
+                    <th scope="row">{{ tableData.data[tableData.data.length - 1][0] }}</th>
+                    <td
+                      v-for="(item, j) in tableData.data[tableData.data.length - 1].slice(
+                        1,
+                        tableData.data.length
+                      )"
+                      :key="j"
+                      class="py-1 lg:text-lg text-center"
+                    >
+                      {{ item }}
+                    </td>
+                  </tr>
+                </tfoot>
+                <caption align="bottom" class="text-sm">
+                  Table 1. Reagent Volumes for Scaffold:Staple Ratios.
+                </caption>
+              </table>
+            </div>
+            <p>
+              Using a thermocycler to cause the scaffold and staple strands to anneal and form a
+              structure, a thermoramp was started by following the instructions listed below to ensure
+              the formation of the structure (Perrault & Shih, 2014) .
+            </p>
+            <ol class="list-decimal list-inside relative left-8 w-fit">
+              <li>80 °C for 5 min</li>
+              <li>Decrease to 65 °C at a rate of -1°C per 5 minutes.</li>
+              <li>Incubate at 65 °C for 20 min</li>
+              <li>Decrease to 25 °C at -1°C per 20 minutes</li>
+            </ol>
+            <p>
+              Finally, to verify the structure that was formed, gel electrophoresis was utilized. A 1%
+              agarose gel was made by adding 0.4 - 0.5 g agarose to 50ml of 0.5X TBE with 10mM
+              MgCl<sub>2</sub> in an erlenmeyer flask. This was microwaved for 15 seconds, swirling
+              and repeated until a homogenous mixture was formed. A gel cast tray of 50 ml was
+              prepared and once the gel had cooled, 5 μl of 10X Sybr safe was pipetted into the gel
+              mix which was poured into the gel cast tray. The gel electrophoresis was run in a 0.5X
+              TBE buffer with 10mM MgCl<sub>2</sub> for 3 hours at 60V.
+            </p>
           </div>
-
-          <p>
-            Using a thermocycler to cause the scaffold and staple strands to anneal and form a
-            structure, a thermoramp was started by following the instructions listed below to ensure
-            the formation of the structure (Perrault & Shih, 2014) .
-          </p>
-          <ol class="list-decimal list-inside relative left-8 w-fit">
-            <li>80 °C for 5 min</li>
-            <li>Decrease to 65 °C at a rate of -1°C per 5 minutes.</li>
-            <li>Incubate at 65 °C for 20 min</li>
-            <li>Decrease to 25 °C at -1°C per 20 minutes</li>
-          </ol>
-          <p>
-            Finally, to verify the structure that was formed, gel electrophoresis was utilized. A 1%
-            agarose gel was made by adding 0.4 - 0.5 g agarose to 50ml of 0.5X TBE with 10mM
-            MgCl<sub>2</sub> in an erlenmeyer flask. This was microwaved for 15 seconds, swirling
-            and repeated until a homogenous mixture was formed. A gel cast tray of 50 ml was
-            prepared and once the gel had cooled, 5 μl of 10X Sybr safe was pipetted into the gel
-            mix which was poured into the gel cast tray. The gel electrophoresis was run in a 0.5X
-            TBE buffer with 10mM MgCl<sub>2</sub> for 3 hours at 60V.
-          </p>
         </template>
       </SingleColumn>
 
